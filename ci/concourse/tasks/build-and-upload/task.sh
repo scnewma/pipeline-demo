dde42b5d02f0697d7a61438ada1833a65573e321
Shaun Newman <scnewmadev@live.com>
2017-10-28 21:34:01 -0700

    adds job to pipeline to run maven build

--- ci/concourse/tasks/build-and-upload/task.sh
+++ ci/concourse/tasks/build-and-upload/task.sh
@@ -2,27 +2,20 @@
 
 set -e
 
-export ROOT_FOLDER=$( pwd )
+export ROOT_FOLDER=$(pwd)
 export REPO_RESOURCE=repo
-export TOOLS_RESOURCE=tools
 export VERSION_RESOURCE=version
 export OUTPUT_RESOURCE=out
 
-export MAVENW_BIN
-MAVENW_BIN="${MAVENW_BIN:-./mvnw}"
+echo "Retrieving version"
+export PIPELINE_VERSION=$(cat ${VERSION_RESOURCE}/version)
+echo "Retrieved version is [${PIPELINE_VERSION}]"
 
-# It takes ages on Docker to run the app without this
-if [[ ${BUILD_OPTIONS} != *"java.security.egd"* ]]; then
-	if [[ ! -z ${BUILD_OPTIONS} && ${BUILD_OPTIONS} != "null" ]]; then
-		export BUILD_OPTIONS="${BUILD_OPTIONS} -Djava.security.egd=file:///dev/urandom"
-	else
-		export BUILD_OPTIONS="-Djava.security.egd=file:///dev/urandom"
-	fi
-fi
+pushd ${ROOT_FOLDER}/${REPO_RESOURCE}
+    ./mvnw org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion="${PIPELINE_VERSION}" || (echo "Build failed!!!" && return 1)
+    ./mvnw clean verify
 
-"${MAVENW_BIN}" org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion="${PIPELINE_VERSION}" || (echo "Build failed!!!" && return 1)
-"${MAVENW_BIN}" clean verify
-
-echo "Tagging the project with dev tag"
-echo "dev/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
-cp -r ${ROOT_FOLDER}/${REPO_RESOURCE}/. ${ROOT_FOLDER}/${OUTPUT_RESOURCE}/
+    echo "Tagging the project with dev tag"
+    echo "dev/${PIPELINE_VERSION}" > ${ROOT_FOLDER}/${REPO_RESOURCE}/tag
+    cp -r ${ROOT_FOLDER}/${REPO_RESOURCE}/. ${ROOT_FOLDER}/${OUTPUT_RESOURCE}/
+popd
